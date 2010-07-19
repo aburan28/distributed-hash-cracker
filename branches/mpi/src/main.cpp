9a169666e2d1c0f63cdd1d82860ca9dc109a16e7
azonenberg <azonenberg@0ab204b9-ead1-e4e3-3bef-21e6378f9c5b>
2010-07-19 04:43:31 +0000

    Added basic high level control flo

--- branches/mpi/src/main.cpp
+++ branches/mpi/src/main.cpp
@@ -39,16 +39,125 @@
  
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void* aligned_malloc(size_t size);
+void aligned_free(void* ptr);
+
+unsigned int* read_hashes(long* pLinecount, const char* fname, int rank);
 
 int main(int argc, char* argv[])
 {
-	MPI_Init(&argc, &argv);
-	
+	//Standard init
 	int rank;
+	int size;
+	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	printf("Hai from %d\n", rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
+	//Parse arguments
+	const char* fname = "testvectors.txt";
+	int maxlength = 6;
+	
+	//Read input
+	long linecount = 0;
+	unsigned int* hashbuf = read_hashes(&linecount, fname, rank);
+	
+	//TODO: preprocess (subtract constants etc)
+	
+	for(int len = 1; len < maxlength; len++)
+	{
+		/*
+			Find our block's start and end values
+			Divide into groups of ~10M guesses
+			For each group:
+				Fill known areas of md5 buffer
+				For each guess:
+					generate plaintexts
+					hash plaintexts
+					check plaintexts against list of hashes, add successes to list if needed
+				Print out successes to logfile
+				Send all of our successes out to other nodes and get their successes
+				Remove successful plaintexts from target list
+		 */					
+	}
+	
+	//Clean up
+	aligned_free(hashbuf);
 	MPI_Finalize();
-	
 	return 0;
+}
+
+/**
+	@brief Allocates memory aligned on a 16 byte boundary
+ */
+void* aligned_malloc(size_t size)
+{
+	//32 = 16 + sizeof(void*) rounded to next 16 bytes
+	void* ptr = malloc(size + 32);
+
+	uintptr_t nptr = (reinterpret_cast<uintptr_t>(ptr) + 32) & (~0xf);
+	*(reinterpret_cast<void**>(nptr) - 1) = ptr;	
+	return reinterpret_cast<void*>(nptr);
+}
+
+/**
+	@brief Frees memory allocated by aligned_malloc()
+ */
+void aligned_free(void* ptr)
+{
+	free(*(reinterpret_cast<void**>(ptr) - 1));
+}
+
+/**
+	@brief Reads hashes from disk
+ */
+unsigned int* read_hashes(long* pLinecount, const char* fname, int rank)
+{
+
+	//Read input file
+	//We expect 33 bytes per line (32 hex + \n)
+	FILE* fp = fopen(fname, "r");
+	if(!fp)
+	{
+		printf("file open error\n");
+		MPI_Abort(MPI_COMM_WORLD, 0);
+	}
+	if(0 != fseek(fp, 0, SEEK_END))
+	{
+		printf("file seek error\n");
+		MPI_Abort(MPI_COMM_WORLD, 0);
+	}
+	long count = ftell(fp);
+	if(0 != fseek(fp, 0, SEEK_SET))
+	{
+		printf("file seek error\n");
+		MPI_Abort(MPI_COMM_WORLD, 0);
+	}
+	long linecount = ceil(static_cast<float>(count) / 33);
+	if(rank == 0)
+		printf("Reading %ld hashes from disk... ", linecount);
+	unsigned int* hashbuf = static_cast<unsigned int*>(aligned_malloc(count * 4));
+	for(long i=0; i<linecount; i++)
+	{
+		unsigned int* base = hashbuf + (i*4);
+		unsigned char* pbase = reinterpret_cast<unsigned char*>(base);
+		unsigned int x;
+		for(int j=0; j<16; j++)
+		{
+			if(1 != fscanf(fp, "%02x", &x))
+			{
+				printf("Malformed input on line %ld\n", i);
+				MPI_Abort(MPI_COMM_WORLD, 0);
+			}
+			pbase[j] = x & 0xff;
+		}
+	}
+	fclose(fp);
+	
+	printf("done\n");
+	
+	*pLinecount = linecount;
+	return hashbuf;
 }
