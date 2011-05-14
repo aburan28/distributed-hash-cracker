d43cf05500c8979d94f39b689ece4ce2e4ea7961
azonenberg@drawersteak.com <azonenberg@drawersteak.com@0ab204b9-ead1-e4e3-3bef-21e6378f9c5b>
2011-05-14 09:53:42 +0000

    Added initial version of guess generation module. Always starts from zero and doesn't know when to stop. Generates one guess per cycle at 175 MHz on an XC6SLX16.

--- branches/fpga/fpgacrack/md5cracker.v
+++ branches/fpga/fpgacrack/md5cracker.v
@@ -5,13 +5,52 @@
 	@brief Top-level module for PoC cracker
 	
  */
-module md5cracker(targetA, targetB, targetC, targetD, clk, hit);
+module md5cracker(targetA, targetB, targetC, targetD, clk, hit, done, guess);
 
-	 input wire [31:0] targetA;
-	 input wire [31:0] targetB;
-	 input wire [31:0] targetC;
-	 input wire [31:0] targetD;
-    input wire clk;
-    output wire hit;	
+	input wire [31:0] targetA;
+	input wire [31:0] targetB;
+	input wire [31:0] targetC;
+	input wire [31:0] targetD;
+   input wire clk;
+   output wire hit;
+	output wire done;
+	
+	output wire[127:0] guess;
+	
+	//Inputs for guess generation
+	reg reset;
+	reg[2:0] charset;
+	reg[4:0] guesslen;
+	
+	//Reset on startup
+	initial begin
+		reset <= 1'd1;
+		charset <= 3'd0;
+		guesslen <= 5'd2;	//2 characters
+	end
+	
+	always @(posedge clk) begin
+		
+		//Done with reset
+		if(reset == 1'd1) begin
+			reset <= 1'd0;
+		end
+		
+		//Normal stuff
+		else begin
+		end
+		
+	end
+	 
+	// Instantiate the module
+	GuessGenerator generator (
+		 .clk(clk),
+		 .charset(charset), 
+		 .guesslen(guesslen), 
+		 .reset(reset), 
+		 .guess(guess),
+		 .done(done)
+		 );
+
 
 endmodule
