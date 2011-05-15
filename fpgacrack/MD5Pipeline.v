43cc7326ed16f62c6afb8f6d1efbe33c9304bedc
azonenberg <azonenberg@drawersteak.com>
2011-05-15 08:08:10 +0000

    Implemented MD5 round 2

--- fpgacrack/MD5Pipeline.v
+++ fpgacrack/MD5Pipeline.v
@@ -134,31 +134,16 @@ module MD5Pipeline(clk, guess, guesslen, hashA, hashB, hashC, hashD
 	
 	//DEBUG: Copy the output
 	always @(posedge clk) begin
-		$display("END OF STEP 1:  %x, %x, %x, %x", digA[1], digB[1], digC[1], digD[1]);
-		$display("END OF STEP 2:  %x, %x, %x, %x", digA[2], digB[2], digC[2], digD[2]);
-		$display("END OF STEP 3:  %x, %x, %x, %x", digA[3], digB[3], digC[3], digD[3]);
-		$display("END OF STEP 4:  %x, %x, %x, %x", digA[4], digB[4], digC[4], digD[4]);
-		$display("END OF STEP 5:  %x, %x, %x, %x", digA[5], digB[5], digC[5], digD[5]);
-		$display("END OF STEP 6:  %x, %x, %x, %x", digA[6], digB[6], digC[6], digD[6]);
-		$display("END OF STEP 7:  %x, %x, %x, %x", digA[7], digB[7], digC[7], digD[7]);
-		$display("END OF STEP 8:  %x, %x, %x, %x", digA[8], digB[8], digC[8], digD[8]);
-		$display("END OF STEP 9:  %x, %x, %x, %x", digA[9], digB[9], digC[9], digD[9]);
-		$display("END OF STEP 10:  %x, %x, %x, %x", digA[10], digB[10], digC[10], digD[10]);
-		$display("END OF STEP 11:  %x, %x, %x, %x", digA[11], digB[11], digC[11], digD[11]);
-		$display("END OF STEP 12:  %x, %x, %x, %x", digA[12], digB[12], digC[12], digD[12]);
-		$display("END OF STEP 13:  %x, %x, %x, %x", digA[13], digB[13], digC[13], digD[13]);
-		$display("END OF STEP 14:  %x, %x, %x, %x", digA[14], digB[14], digC[14], digD[14]);
-		$display("END OF STEP 15:  %x, %x, %x, %x", digA[15], digB[15], digC[15], digD[15]);
 		$display("END OF ROUND 1:  %x, %x, %x, %x", digA[16], digB[16], digC[16], digD[16]);
+		$display("END OF ROUND 2:  %x, %x, %x, %x", digA[32], digB[32], digC[32], digD[32]);
 		
-		hashA <= digA[16];
-		hashB <= digB[16];
-		hashC <= digC[16];
-		hashD <= digD[16];
+		hashA <= digA[32];
+		hashB <= digB[32];
+		hashC <= digC[32];
+		hashD <= digD[32];
 	end
 	
-	//FIRST ROUND
-	wire[31:0] outA1_0;
+	//ROUND 1
 	MD5RoundF #(.s(5'd7), .p(0)) round1_0(clk, digA[0], digB[0], digC[0], digD[0], buf0[0], 32'hd76aa478, digA[1]);
 		assign digB[1] = digB[0];
 		assign digC[1] = digC[0];
@@ -223,6 +208,73 @@ module MD5Pipeline(clk, guess, guesslen, hashA, hashB, hashC, hashD
 		assign digD[16] = digD[15];
 		assign digA[16] = digA[15]; 
 		assign digC[16] = digC[15];
+		
+	//ROUND 2
+	MD5RoundG #(.s(5'd5), .p(0)) round2_0(clk, digA[16], digB[16], digC[16], digD[16], buf1[16], 32'hf61e2562, digA[17]);
+		assign digB[17] = digB[16];
+		assign digC[17] = digC[16];
+		assign digD[17] = digD[16];
+	MD5RoundG #(.s(5'd9), .p(0)) round2_1(clk, digD[17], digA[17], digB[17], digC[17], 32'h0, 32'hc040b340, digD[18]);
+		assign digB[18] = digB[17];
+		assign digC[18] = digC[17];
+		assign digA[18] = digA[17]; 
+	MD5RoundG #(.s(5'd14), .p(0)) round2_2(clk, digC[18], digD[18], digA[18], digB[18], 32'h0, 32'h265e5a51, digC[19]);
+		assign digD[19] = digD[18];
+		assign digA[19] = digA[18]; 
+		assign digB[19] = digB[18];
+	MD5RoundG #(.s(5'd20), .p(0)) round2_3(clk, digB[19], digC[19], digD[19], digA[19], buf0[19], 32'he9b6c7aa, digB[20]);
+		assign digD[20] = digD[19];
+		assign digA[20] = digA[19]; 
+		assign digC[20] = digC[19];
+	MD5RoundG #(.s(5'd5), .p(0)) round2_4(clk, digA[20], digB[20], digC[20], digD[20], 32'h0, 32'hd62f105d, digA[21]);
+		assign digB[21] = digB[20];
+		assign digC[21] = digC[20];
+		assign digD[21] = digD[20];
+	MD5RoundG #(.s(5'd9), .p(0)) round2_5(clk, digD[21], digA[21], digB[21], digC[21], 32'h0, 32'h02441453, digD[22]);
+		assign digB[22] = digB[21];
+		assign digC[22] = digC[21];
+		assign digA[22] = digA[21]; 
+	MD5RoundG #(.s(5'd14), .p(0)) round2_6(clk, digC[22], digD[22], digA[22], digB[22], 32'h0, 32'hd8a1e681, digC[23]);
+		assign digD[23] = digD[22];
+		assign digA[23] = digA[22]; 
+		assign digB[23] = digB[22];
+	MD5RoundG #(.s(5'd20), .p(0)) round2_7(clk, digB[23], digC[23], digD[23], digA[23], 32'h0, 32'he7d3fbc8, digB[24]);
+		assign digD[24] = digD[23];
+		assign digA[24] = digA[23]; 
+		assign digC[24] = digC[23];
+	MD5RoundG #(.s(5'd5), .p(0)) round2_8(clk, digA[24], digB[24], digC[24], digD[24], 32'h0, 32'h21e1cde6, digA[25]);
+		assign digB[25] = digB[24];
+		assign digC[25] = digC[24];
+		assign digD[25] = digD[24];
+	MD5RoundG #(.s(5'd9), .p(0)) round2_9(clk, digD[25], digA[25], digB[25], digC[25], buf14[25], 32'hc33707d6, digD[26]);
+		assign digB[26] = digB[25];
+		assign digC[26] = digC[25];
+		assign digA[26] = digA[25]; 
+	MD5RoundG #(.s(5'd14), .p(0)) round2_10(clk, digC[26], digD[26], digA[26], digB[26], buf3[26], 32'hf4d50d87, digC[27]);
+		assign digD[27] = digD[26];
+		assign digA[27] = digA[26]; 
+		assign digB[27] = digB[26];
+	MD5RoundG #(.s(5'd20), .p(0)) round2_11(clk, digB[27], digC[27], digD[27], digA[27], 32'h0, 32'h455a14ed, digB[28]);
+		assign digD[28] = digD[27];
+		assign digA[28] = digA[27]; 
+		assign digC[28] = digC[27];
+	MD5RoundG #(.s(5'd5), .p(0)) round2_12(clk, digA[28], digB[28], digC[28], digD[28], 32'h0, 32'ha9e3e905, digA[29]);
+		assign digB[29] = digB[28];
+		assign digC[29] = digC[28];
+		assign digD[29] = digD[28];
+	MD5RoundG #(.s(5'd9), .p(0)) round2_13(clk, digD[29], digA[29], digB[29], digC[29], buf2[29], 32'hfcefa3f8, digD[30]);
+		assign digB[30] = digB[29];
+		assign digC[30] = digC[29];
+		assign digA[30] = digA[29]; 
+	MD5RoundG #(.s(5'd14), .p(0)) round2_14(clk, digC[30], digD[30], digA[30], digB[30], 32'h0, 32'h676f02d9, digC[31]);
+		assign digD[31] = digD[30];
+		assign digA[31] = digA[30]; 
+		assign digB[31] = digB[30];
+	MD5RoundG #(.s(5'd20), .p(0)) round2_15(clk, digB[31], digC[31], digD[31], digA[31], 32'h0, 32'h8d2a4c8a, digB[32]);
+		assign digD[32] = digD[31];
+		assign digA[32] = digA[31]; 
+		assign digC[32] = digC[31];
+	
 
 endmodule
 
@@ -260,6 +312,64 @@ module MD5RoundF(clk, a, b, c, d, k, t, out);
 	wire[31:0] f;
 	wire[31:0] inner;
 	assign f = (b & c) | (~b & d);
+	assign inner = a + f + k + t;
+	
+	wire[63:0] xrrotval = ({inner, inner} << s);
+	wire[31:0] rrotval = xrrotval[63:32];
+	
+	//"Funnel shift" for left rotate
+	always @(posedge clk) begin
+		if(p) begin
+			$display("------");
+			$display("  A = %x", a);
+			$display("  B = %x", b);
+			$display("  C = %x", c);
+			$display("  D = %x", d);
+			$display("  X = %x", k);
+			$display("  T = %x", t);
+			$display("  fval = %x", f);
+			$display("  rotval = %x", inner);
+			$display("  rrotval = %x", rrotval);
+		end
+		out <= b + rrotval;
+	end
+	
+endmodule
+
+/**
+	@brief Does an G round
+	
+	@param clk		Clock
+	@param a			Current A word
+	@param b			Current B word
+	@param c			Current C word
+	@param d			Current D word
+	@param k			Word #K from the message
+	@param t			Input constant T
+	
+	@param s			Shift amount
+ */
+module MD5RoundG(clk, a, b, c, d, k, t, out);
+	input wire clk;
+	input wire[31:0] a;
+	input wire[31:0] b;
+	input wire[31:0] c;
+	input wire[31:0] d;
+	input wire[31:0] k;
+	input wire[31:0] t;
+	output reg[31:0] out;
+	
+	initial begin
+		out <= 32'd0;
+	end
+	
+	parameter s = 7;		//shift value
+	parameter p = 0;		//true to print
+
+	//Core round function
+	wire[31:0] f;
+	wire[31:0] inner;
+	assign f = (b & d) | (~d & c);
 	assign inner = a + f + k + t;
 	
 	wire[63:0] xrrotval = ({inner, inner} << s);
